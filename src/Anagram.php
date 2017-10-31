51385777bd281166f2a15290d540d1758a82c7bb
Michael Gwynne <michael.gwynne@bbc.co.uk>
2017-10-31 14:15:29 +0000

    Mike

--- src/Anagram.php
+++ src/Anagram.php
@@ -4,7 +4,30 @@ class Anagram
 {
     public static function isAnagram(string $word1, string $word2): bool
     {
-        // @TODO Write code to check if two words are anagrams
-        return false;
+        if (strlen($word1) !== strlen($word2)) {
+            return false;
+        }
+
+        $letterCount = [];
+
+        foreach(str_split($word1) as $letter) {
+            if(isset($letterCount[$letter])) {
+                ++$letterCount[$letter];
+            } else {
+                $letterCount[$letter] = 1;
+            }
+        }
+
+        foreach(str_split($word2) as $letter) {
+            if (!isset($letterCount[$letter])) {
+                return false;
+            }
+            --$letterCount[$letter];
+            if($letterCount[$letter] === 0) {
+                unset($letterCount[$letter]);
+            }
+        }
+
+        return empty($letterCount);
     }
 }
