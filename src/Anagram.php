0f0e8d48cd7ddb92b4be9df623d7ac1b400b1673
Matt Greenham <matt.greenham@bbc.co.uk>
2017-10-31 14:34:10 +0000

    Matt's inefficient code

--- src/Anagram.php
+++ src/Anagram.php
@@ -4,7 +4,32 @@ class Anagram
 {
     public static function isAnagram(string $word1, string $word2): bool
     {
-        // @TODO Write code to check if two words are anagrams
-        return false;
+        if (strlen($word1) !== strlen($word2)) {
+            return false;
+        }
+
+        $length = strlen($word1);
+
+        $word1 = strtoupper($word1);
+        $word2 = strtoupper($word2);
+
+        $firstWord = [];
+        $secondWord = [];
+
+        for ($i = 0; $i < $length; $i++) {
+            $firstWord[$i] = ord(substr($word1, $i, 1));
+            $secondWord[$i] = ord(substr($word2, $i, 1));
+        }
+
+        sort($firstWord);
+        sort($secondWord);
+
+        for ($i = 0; $i < $length; $i++) {
+            if ($firstWord[$i] !== $secondWord[$i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
